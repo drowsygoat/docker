e5b715773713eb8988657aa55ed3e676b0b0cf29
Lechu <32292322+drowsygoat@users.noreply.github.com>
2024-09-04 11:22:37 +0200

    R412

--- R_4.1.2/entrypoint.sh
+++ R_4.1.2/entrypoint.sh
@@ -4,7 +4,11 @@
 if [ $# -eq 0 ]; then
     exec /bin/bash
 else
-    # Otherwise, run the provided command
-    exec "$@"
+    # If the first argument is "rstudio", start RStudio Server
+    if [ "$1" == "rstudio" ]; then
+        exec /usr/lib/rstudio-server/bin/rserver --server-daemonize 0
+    else
+        # Otherwise, run the provided command
+        exec "$@"
+    fi
 fi
-
